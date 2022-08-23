493d3a2dc26291d0815598285e00b05d19ae5f02
Paul Tarvydas <guitarvydas@gmail.com>
2022-08-23 06:30:52 -0400

    hello.py and world.py init'ed with instance name

--- Hello.py
+++ Hello.py
@@ -5,6 +5,6 @@ class Hello (Procedure):
     def f1 (self, message):
         self.send ('stdout', 'hello', message)
 
-    def __init__ (self, parent):
+    def __init__ (self, parent, name):
         h1 = PortHandler ('', self.f1)
-        super ().__init__ (parent=parent, name='hello', portHandler=h1)
+        super ().__init__ (parent=parent, name=name, portHandler=h1)
--- test.py
+++ test.py
@@ -2,14 +2,14 @@ from message import Message
 from hello import Hello
 from world import World
 
-h = Hello (None)
+h = Hello (None, 'Hello instance')
 h.inject (Message (h, '', True, None))
 h.run ()
 print (h.outputs ())
 
 firstoutput = h.outputs () ['stdout'][0]
 
-w = World (None)
+w = World (None, 'World instance')
 w.inject (Message (w, '', firstoutput, None))
 w.run ()
 print (w.outputs ())
