67fa425b8680d8629b5538cabb00df770d7ec8cc
Paul Tarvydas <guitarvydas@gmail.com>
2022-08-22 23:42:39 -0400

    world.py echoes its message.data, then send "world" (2 sends for 1 input)

--- test.py
+++ test.py
@@ -1,8 +1,14 @@
 from message import Message
 from hello import Hello
+from world import World
 
 e = Hello (None)
 e.inject (Message (e, '', True, None))
+e.run ()
+print (e.outputs ())
+
+e = World (None)
+e.inject (Message (e, '', 'hello', None))
 e.run ()
 print (e.outputs ())
 
