c3a9aaff64af5cb1f5310abd7c2bbc6ac93d7264
Paul Tarvydas <guitarvydas@gmail.com>
2022-08-26 08:02:13 -0400

    copy/pasted das as subtree

---
+++ das/fb.pl
@@ -0,0 +1,269 @@
+diagram_fact(align, cell_8, "left").
+diagram_fact(as, cell_10, "geometry").
+diagram_fact(as, cell_11, "geometry").
+diagram_fact(as, cell_12, "geometry").
+diagram_fact(as, cell_13, "geometry").
+diagram_fact(as, cell_14, "geometry").
+diagram_fact(as, cell_6, "geometry").
+diagram_fact(as, cell_7, "geometry").
+diagram_fact(as, cell_8, "geometry").
+diagram_fact(as, cell_9, "geometry").
+diagram_fact(aspect, cell_10, "fixed").
+diagram_fact(aspect, cell_12, "fixed").
+diagram_fact(aspect, cell_13, "fixed").
+diagram_fact(aspect, cell_14, "fixed").
+diagram_fact(cell, cell_10, "").
+diagram_fact(cell, cell_11, "").
+diagram_fact(cell, cell_12, "").
+diagram_fact(cell, cell_13, "").
+diagram_fact(cell, cell_14, "").
+diagram_fact(cell, cell_4, "").
+diagram_fact(cell, cell_5, "").
+diagram_fact(cell, cell_6, "").
+diagram_fact(cell, cell_7, "").
+diagram_fact(cell, cell_8, "").
+diagram_fact(cell, cell_9, "").
+diagram_fact(diagram, diagram_1, "").
+diagram_fact(edge, cell_11, 1).
+diagram_fact(edge, cell_9, 1).
+diagram_fact(edgeStyle, cell_11, "orthogonalEdgeStyle").
+diagram_fact(edgeStyle, cell_9, "orthogonalEdgeStyle").
+diagram_fact(entryDx, cell_11, 0).
+diagram_fact(entryDx, cell_9, 0).
+diagram_fact(entryDy, cell_11, 0).
+diagram_fact(entryDy, cell_9, 0).
+diagram_fact(entryX, cell_11, 0).
+diagram_fact(entryX, cell_9, 0).
+diagram_fact(entryY, cell_11, 0.5).
+diagram_fact(entryY, cell_9, 0.5).
+diagram_fact(exitDx, cell_11, 0).
+diagram_fact(exitDx, cell_9, 0).
+diagram_fact(exitDy, cell_11, 0).
+diagram_fact(exitDy, cell_9, 0).
+diagram_fact(exitX, cell_11, 1).
+diagram_fact(exitX, cell_9, 1).
+diagram_fact(exitY, cell_11, 0.5).
+diagram_fact(exitY, cell_9, 0.5).
+diagram_fact(fillColor, cell_10, "#fff2cc").
+diagram_fact(fillColor, cell_12, "#d5e8d4").
+diagram_fact(fillColor, cell_13, "#d5e8d4").
+diagram_fact(fillColor, cell_14, "#fff2cc").
+diagram_fact(fillColor, cell_7, "#dae8fc").
+diagram_fact(fillColor, cell_8, "#f8cecc").
+diagram_fact(gradientColor, cell_7, "#7ea6e0").
+diagram_fact(height, cell_10, 40).
+diagram_fact(height, cell_12, 40).
+diagram_fact(height, cell_13, 40).
+diagram_fact(height, cell_14, 40).
+diagram_fact(height, cell_6, 230).
+diagram_fact(height, cell_7, 130).
+diagram_fact(height, cell_8, 45).
+diagram_fact(html, cell_10, 1).
+diagram_fact(html, cell_11, 1).
+diagram_fact(html, cell_12, 1).
+diagram_fact(html, cell_13, 1).
+diagram_fact(html, cell_14, 1).
+diagram_fact(html, cell_6, 1).
+diagram_fact(html, cell_7, 1).
+diagram_fact(html, cell_8, 1).
+diagram_fact(html, cell_9, 1).
+diagram_fact(jettySize, cell_11, "auto").
+diagram_fact(jettySize, cell_9, "auto").
+diagram_fact(kind, cell_10, "ellipse").
+diagram_fact(kind, cell_12, "ellipse").
+diagram_fact(kind, cell_13, "ellipse").
+diagram_fact(kind, cell_14, "ellipse").
+diagram_fact(mxGraphModel, mxGraphModel_2, mxGraphModel_2).
+diagram_fact(name, diagram_1, "hw").
+diagram_fact(orthogonalLoop, cell_11, 1).
+diagram_fact(orthogonalLoop, cell_9, 1).
+diagram_fact(parent, cell_10, id_1).
+diagram_fact(parent, cell_11, id_1).
+diagram_fact(parent, cell_12, id_1).
+diagram_fact(parent, cell_13, id_1).
+diagram_fact(parent, cell_14, id_1).
+diagram_fact(parent, cell_5, id_0).
+diagram_fact(parent, cell_6, id_1).
+diagram_fact(parent, cell_7, id_1).
+diagram_fact(parent, cell_8, id_1).
+diagram_fact(parent, cell_9, id_1).
+diagram_fact(relative, cell_11, 1).
+diagram_fact(relative, cell_9, 1).
+diagram_fact(root, root_3, root_3).
+diagram_fact(rounded, cell_11, 0).
+diagram_fact(rounded, cell_6, 1).
+diagram_fact(rounded, cell_7, 1).
+diagram_fact(rounded, cell_8, 0).
+diagram_fact(rounded, cell_9, 0).
+diagram_fact(source, cell_11, id_Eg2JflB1LH7_nM4HlBUV__1).
+diagram_fact(source, cell_9, i5jZ3dy8F4nkPbaXPHF6__5).
+diagram_fact(strokeColor, cell_10, "#d6b656").
+diagram_fact(strokeColor, cell_12, "#82b366").
+diagram_fact(strokeColor, cell_13, "#82b366").
+diagram_fact(strokeColor, cell_14, "#d6b656").
+diagram_fact(strokeColor, cell_7, "#6c8ebf").
+diagram_fact(strokeColor, cell_8, "#b85450").
+diagram_fact(synonym, cell_10, i5jZ3dy8F4nkPbaXPHF6__5).
+diagram_fact(synonym, cell_11, id_Eg2JflB1LH7_nM4HlBUV__3).
+diagram_fact(synonym, cell_12, id_Eg2JflB1LH7_nM4HlBUV__1).
+diagram_fact(synonym, cell_13, id_Eg2JflB1LH7_nM4HlBUV__2).
+diagram_fact(synonym, cell_14, cVRvWxy9wkcLxyWbIS20__3).
+diagram_fact(synonym, cell_4, id_0).
+diagram_fact(synonym, cell_5, id_1).
+diagram_fact(synonym, cell_6, i5jZ3dy8F4nkPbaXPHF6__1).
+diagram_fact(synonym, cell_7, i5jZ3dy8F4nkPbaXPHF6__2).
+diagram_fact(synonym, cell_8, i5jZ3dy8F4nkPbaXPHF6__3).
+diagram_fact(synonym, cell_9, __D01R01AcrLnmJQQSoYk__1).
+diagram_fact(target, cell_11, id_Eg2JflB1LH7_nM4HlBUV__2).
+diagram_fact(target, cell_9, cVRvWxy9wkcLxyWbIS20__3).
+diagram_fact(value, cell_10, "outh").
+diagram_fact(value, cell_12, "inDummy").
+diagram_fact(value, cell_13, "inh").
+diagram_fact(value, cell_14, "outDummy").
+diagram_fact(value, cell_6, "dummy").
+diagram_fact(value, cell_7, "hello").
+diagram_fact(value, cell_8, "&lt;br&gt;&lt;div&gt;&lt;/div&gt;").
+diagram_fact(vertex, cell_10, 1).
+diagram_fact(vertex, cell_12, 1).
+diagram_fact(vertex, cell_13, 1).
+diagram_fact(vertex, cell_14, 1).
+diagram_fact(vertex, cell_6, 1).
+diagram_fact(vertex, cell_7, 1).
+diagram_fact(vertex, cell_8, 1).
+diagram_fact(verticalAlign, cell_6, "top").
+diagram_fact(verticalAlign, cell_7, "top").
+diagram_fact(whiteSpace, cell_10, "wrap").
+diagram_fact(whiteSpace, cell_12, "wrap").
+diagram_fact(whiteSpace, cell_13, "wrap").
+diagram_fact(whiteSpace, cell_14, "wrap").
+diagram_fact(whiteSpace, cell_6, "wrap").
+diagram_fact(whiteSpace, cell_7, "wrap").
+diagram_fact(whiteSpace, cell_8, "wrap").
+diagram_fact(width, cell_10, 40).
+diagram_fact(width, cell_12, 40).
+diagram_fact(width, cell_13, 40).
+diagram_fact(width, cell_14, 40).
+diagram_fact(width, cell_6, 540).
+diagram_fact(width, cell_7, 260).
+diagram_fact(width, cell_8, 200).
+diagram_fact(x, cell_10, 470).
+diagram_fact(x, cell_12, 100).
+diagram_fact(x, cell_13, 210).
+diagram_fact(x, cell_14, 640).
+diagram_fact(x, cell_6, 120).
+diagram_fact(x, cell_7, 230).
+diagram_fact(x, cell_8, 260).
+diagram_fact(y, cell_10, 265).
+diagram_fact(y, cell_12, 265).
+diagram_fact(y, cell_13, 265).
+diagram_fact(y, cell_14, 265).
+diagram_fact(y, cell_6, 170).
+diagram_fact(y, cell_7, 220).
+diagram_fact(y, cell_8, 265).
+das_fact(kind,cell_10,ellipse).
+das_fact(kind,cell_11,edge).
+das_fact(kind,cell_12,ellipse).
+das_fact(kind,cell_13,ellipse).
+das_fact(kind,cell_14,ellipse).
+das_fact(kind,cell_4,rectangle).
+das_fact(kind,cell_5,rectangle).
+das_fact(kind,cell_6,rectangle).
+das_fact(kind,cell_7,rectangle).
+das_fact(kind,cell_8,rectangle).
+das_fact(kind,cell_9,edge).
+das_fact(name, cell_10, "outh").
+das_fact(name, cell_12, "inDummy").
+das_fact(name, cell_13, "inh").
+das_fact(name, cell_14, "outDummy").
+das_fact(name, cell_6, "dummy").
+das_fact(name, cell_7, "hello").
+das_fact(name, cell_8, "&lt;br&gt;&lt;div&gt;&lt;/div&gt;").
+das_fact(color,cell_10,"yellow").
+das_fact(color,cell_11,"-").
+das_fact(color,cell_12,"green").
+das_fact(color,cell_13,"green").
+das_fact(color,cell_14,"yellow").
+das_fact(color,cell_4,"-").
+das_fact(color,cell_5,"-").
+das_fact(color,cell_6,"-").
+das_fact(color,cell_7,"-").
+das_fact(color,cell_8,"red").
+das_fact(color,cell_9,"-").
+das_fact(bbL,cell_10,470).
+  das_fact(bbT,cell_10,265).
+  das_fact(bbR,cell_10,510).
+  das_fact(bbB,cell_10,305).
+das_fact(bbL,cell_12,100).
+  das_fact(bbT,cell_12,265).
+  das_fact(bbR,cell_12,140).
+  das_fact(bbB,cell_12,305).
+das_fact(bbL,cell_13,210).
+  das_fact(bbT,cell_13,265).
+  das_fact(bbR,cell_13,250).
+  das_fact(bbB,cell_13,305).
+das_fact(bbL,cell_14,640).
+  das_fact(bbT,cell_14,265).
+  das_fact(bbR,cell_14,680).
+  das_fact(bbB,cell_14,305).
+das_fact(bbL,cell_6,120).
+  das_fact(bbT,cell_6,170).
+  das_fact(bbR,cell_6,660).
+  das_fact(bbB,cell_6,400).
+das_fact(bbL,cell_7,230).
+  das_fact(bbT,cell_7,220).
+  das_fact(bbR,cell_7,490).
+  das_fact(bbB,cell_7,350).
+das_fact(bbL,cell_8,260).
+  das_fact(bbT,cell_8,265).
+  das_fact(bbR,cell_8,460).
+  das_fact(bbB,cell_8,310).
+das_fact(direction,cell_10,output).
+das_fact(direction,cell_12,input).
+das_fact(direction,cell_13,input).
+das_fact(direction,cell_14,output).
+das_fact(contains,cell_6,cell_10).
+das_fact(contains,cell_6,cell_13).
+das_fact(contains,cell_6,cell_7).
+das_fact(contains,cell_6,cell_8).
+das_fact(contains,cell_7,cell_8).
+das_fact(contains,cell_6,cell_12).
+das_fact(contains,cell_6,cell_14).
+das_fact(contains,cell_7,cell_10).
+das_fact(contains,cell_7,cell_13).
+das_fact(indirect_contains,cell_6,cell_10).
+das_fact(indirect_contains,cell_6,cell_13).
+das_fact(indirect_contains,cell_6,cell_8).
+das_fact(direct_contains,cell_6,cell_12).
+das_fact(direct_contains,cell_6,cell_14).
+das_fact(direct_contains,cell_6,cell_7).
+das_fact(direct_contains,cell_7,cell_10).
+das_fact(direct_contains,cell_7,cell_13).
+das_fact(direct_contains,cell_7,cell_8).
+das_fact(direct_contains,cell_6,cell_9).
+das_fact(direct_contains,cell_6,cell_11).
+das_fact(direct_contains,cell_6,cell_9).
+das_fact(codebox,cell_8,"&lt;br&gt;&lt;div&gt;&lt;/div&gt;").
+das_fact(connection, cell_6, cell_11).
+das_fact(sender, cell_11, sender{component:"dummy",port:"inDummy"}).
+das_fact(receiver, cell_11, receiver{component:"hello",port:"inh"}).
+das_fact(connection, cell_6, cell_11).
+das_fact(sender, cell_11, sender{component:"dummy",port:"inDummy"}).
+das_fact(receiver, cell_11, receiver{component:"dummy",port:"cell_9"}).
+das_fact(connection, cell_6, cell_11).
+das_fact(sender, cell_11, sender{component:"dummy",port:"cell_9"}).
+das_fact(receiver, cell_11, receiver{component:"hello",port:"inh"}).
+das_fact(connection, cell_6, cell_11).
+das_fact(sender, cell_11, sender{component:"dummy",port:"cell_9"}).
+das_fact(receiver, cell_11, receiver{component:"dummy",port:"cell_9"}).
+das_fact(connection, cell_6, cell_9).
+das_fact(sender, cell_9, sender{component:"hello",port:"outh"}).
+das_fact(receiver, cell_9, receiver{component:"dummy",port:"outDummy"}).
+das_fact(connection, cell_6, cell_9).
+das_fact(sender, cell_9, sender{component:"hello",port:"outh"}).
+das_fact(receiver, cell_9, receiver{component:"dummy",port:"cell_9"}).
+das_fact(connection, cell_6, cell_9).
+das_fact(sender, cell_9, sender{component:"dummy",port:"cell_9"}).
+das_fact(receiver, cell_9, receiver{component:"dummy",port:"outDummy"}).
+das_fact(connection, cell_6, cell_9).
+das_fact(sender, cell_9, sender{component:"dummy",port:"cell_9"}).
+das_fact(receiver, cell_9, receiver{component:"dummy",port:"cell_9"}).
