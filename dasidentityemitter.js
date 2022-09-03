2589cedea0a859e8e2145bd382ec233225d0c434
Paul Tarvydas <guitarvydas@gmail.com>
2022-09-03 08:43:23 -0400

    phase Emitter working on stripped-down src

--- dasidentityemitter.js
+++ dasidentityemitter.js
@@ -25,7 +25,7 @@ CField_name [dq1 k dq2 kcolon s] = ‛’
 CField_kind [dq1 k dq2 kcolon s] = ‛⟨dq1⟩⟨k⟩⟨dq2⟩⟨kcolon⟩⟨s⟩’
 CField_outputs [dq1 k dq2 kcolon s] = ‛’
 CField_synccode [dq1 k dq2 kcolon s] = ‛’
-CField_connections [dq1 k dq2 kcolon ConnectionBody] = ‛⟨ConnectionBody⟩’
+CField_connections [dq1 k dq2 kcolon ConnectionBody] = ‛⟨dq1⟩⟨k⟩⟨dq2⟩⟨kcolon⟩[⟨ConnectionBody⟩]’
 
 ConnectionBody [lb Connection* optcomma* rb] = ‛⟨Connection⟩’
 
--- src.js
+++ src.js
@@ -2,19 +2,11 @@ const jsonsrc = String.raw`
 [
   [
     {
-      "children": [ {"kind":"Hello", "name":"cell_7"},  {"kind":"World", "name":"cell_8"} ],
+      "children": [ {"kind":"X", "name":"Y"}, ],
       "connections": [
 	{
 	  "receivers": [ {"receiver": {"component":"cell_7", "port":"b"}} ],
 	  "senders": [ {"sender": {"component":"cell_6", "port":"a"}} ]
-	},
-	{
-	  "receivers": [ {"receiver": {"component":"cell_8", "port":"d"}} ],
-	  "senders": [ {"sender": {"component":"cell_7", "port":"c"}} ]
-	},
-	{
-	  "receivers": [ {"receiver": {"component":"cell_6", "port":"f"}} ],
-	  "senders": [ {"sender": {"component":"cell_8", "port":"e"}} ]
 	}
       ],
       "id":"cell_6",
@@ -22,30 +14,6 @@ const jsonsrc = String.raw`
       "kind":"HelloWorld",
       "name":"HelloWorld",
       "outputs": ["cell_15" ],
-      "synccode":""
-    }
-  ],
-  [
-    {
-      "children": [],
-      "connections": [],
-      "id":"cell_7",
-      "inputs": ["cell_12" ],
-      "kind":"Hello",
-      "name":"Hello",
-      "outputs": ["cell_10" ],
-      "synccode":""
-    }
-  ],
-  [
-    {
-      "children": [],
-      "connections": [],
-      "id":"cell_8",
-      "inputs": ["cell_11" ],
-      "kind":"World",
-      "name":"World",
-      "outputs": ["cell_14" ],
       "synccode":""
     }
   ]
