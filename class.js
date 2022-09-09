76b8b0bd1687a067e4daa11f6b724eb0a059c045
Paul Tarvydas <guitarvydas@gmail.com>
2022-09-08 23:03:24 -0400

    class.js

--- class.js
+++ class.js
@@ -15,6 +15,7 @@ ComponentDef [lb ComponentJSON rb optcomma] = ‛⟨ComponentJSON⟩’
 ComponentJSON [x] = ‛⟨x⟩’
 ComponentContainerJSON [lb NonEmptyChildren ComponentField+ rb] = ‛\nclass ⟨selfKind⟩ (Container): (.
 ⟨NonEmptyChildren⟩
+⟨ComponentField⟩
 super ().__init__ (parent, name, self._children, self._connections)
 .)
 ’
@@ -27,7 +28,7 @@ super ().__init__ (parent, name, null, null)
 EmptyChildren [dq1 kchildren dq2 kcolon lb rb optcomma?] = ‛’
 NonEmptyChildren [dq1 kchildren dq2 kcolon ChildList optcomma?] = ‛⟨ChildList⟩’
 
-ComponentField [CField optcomma?] = ‛’
+ComponentField [CField optcomma?] = ‛⟨CField⟩’
 
 CField_id [dq1 k dq2 kcolon s] = ‛’
 CField_inputs [dq1 k dq2 kcolon s] = ‛’
@@ -35,19 +36,18 @@ CField_name [dq1 k dq2 kcolon s] = ‛’
 CField_kind [dq1 k dq2 kcolon s] = ‛’
 CField_outputs [dq1 k dq2 kcolon s] = ‛’
 CField_synccode [dq1 k dq2 kcolon s] = ‛’
-CField_connections [dq1 k dq2 kcolon ConnectionBody] = ‛’
+CField_connections [dq1 k dq2 kcolon ConnectionBody] = ‛⟨ConnectionBody⟩’
 
-ConnectionBody [lb Connection* optcomma* rb] = ‛’
+ConnectionBody [lb Connection* optcomma* rb] = ‛⟨fmtConnections (Connection)⟩’
 
-Connection [lb Receiver kcomma Sender rb] = ‛’
+Connection [lb Receiver kcomma Sender rb] = ‛\n⟨lb⟩⟨Receiver⟩,⟨Sender⟩⟨rb⟩’
+Receiver [dq1 kreceivers dq2 kcolon1 lbracket lbrace dq3 kreceiver dq4 kcolon2 Pair rbrace rbracket] = ‛⟨dq1⟩⟨kreceivers⟩⟨dq2⟩⟨kcolon1⟩⟨lbracket⟩⟨lbrace⟩⟨dq3⟩⟨kreceiver⟩⟨dq4⟩⟨kcolon2⟩⟨Pair⟩⟨rbrace⟩⟨rbracket⟩’
+Sender  [dq1 ksenders dq2 kcolon1 lbracket lbrace dq3 ksender dq4 kcolon2 Pair rbrace rbracket] = ‛⟨dq1⟩⟨ksenders⟩⟨dq2⟩⟨kcolon1⟩⟨lbracket⟩⟨lbrace⟩⟨dq3⟩⟨ksender⟩⟨dq4⟩⟨kcolon2⟩⟨Pair⟩⟨rbrace⟩⟨rbracket⟩’
 
-Receiver [dq1 kreceivers dq2 kcolon1 lbracket lbrace dq3 kreceiver dq4 kcolon2 Pair rbrace rbracket] = ‛’
-Sender  [dq1 ksenders dq2 kcolon1 lbracket lbrace dq3 ksender dq4 kcolon2 Pair rbrace rbracket] = ‛’
+Pair [lb kwcomponent kcolon1 ComponentName kcomma kwport kcolon2 PortName rb] = ‛⟨lb⟩⟨kwcomponent⟩⟨kcolon1⟩⟨ComponentName⟩⟨kcomma⟩⟨kwport⟩⟨kcolon2⟩⟨PortName⟩⟨rb⟩’
 
-Pair [lb kwcomponent kcolon1 ComponentName kcomma kwport kcolon2 PortName rb] = ‛’
-
-kwcomponent [dq1 kcomponent dq2] = ‛’
-kwport [dq1 kport dq2] = ‛’
+kwcomponent [dq1 kcomponent dq2] = ‛⟨dq1⟩⟨kcomponent⟩⟨dq2⟩’
+kwport [dq1 kport dq2] = ‛⟨dq1⟩⟨kport⟩⟨dq2⟩’
 ComponentName_self [q1 s q2] = ‛"⟨s⟩"’
 ComponentName_name [s] = ‛"⟨s⟩"’
 PortName [s] = ‛"⟨s⟩"’
@@ -63,8 +63,6 @@ dq [c] = ‛⟨c⟩’
 
 }
 `
-//      + fComponents
-//      + fInsert
 + `
 fComponents {
   Components [vs0 lb vs1 Component+ vs2 rb vs3] = ‛⟨vs0⟩⟨vs1⟩⟨Component⟩⟨vs2⟩⟨vs3⟩’
@@ -74,11 +72,6 @@ fSelfDefs {
   SelfKind [kself keq kind KindName] = ‛.kind=⟨KindName⟩⟨selfKind=KindName,""⟩’
 }
 `
-//       + `      
-// fChild {
-//   Child [lb kkind kcolon KindName kcomma kname kcolon ComponentName rb optcomma?] = ‛⟨lb⟩⟨kkind⟩⟨kcolon⟩⟨KindName⟩⟨kcomma⟩⟨kname⟩⟨kcolon⟩⟨ComponentName⟩⟨rb⟩⟨optcomma⟩’
-// }
-// `
       + fChild
       + fVerbatim;
 
@@ -148,3 +141,94 @@ function fmtChild (text) {
     }
 }
 
+
+
+
+///////// connections
+
+// form
+// {"receivers":[{"receiver":{"component":"cell_7","port":"b"}}],"senders":[{"sender":{"component":"cell_6","port":"a"}}]}
+// {"receivers":[{"receiver":{"component":"cell_8","port":"d"}}],"senders":[{"sender":{"component":"cell_7","port":"c"}}]}
+// {"receivers":[{"receiver":{"component":"cell_6","port":"f"}}],"senders":[{"sender":{"component":"cell_8","port":"e"}}]}
+
+const gConnections = String.raw`
+Connections {
+  Connections = Connection*
+  Connection := 
+    | "{" SelfReceiver "," SelfSender "}" -- passThrough
+    | "{" Receiver "," SelfSender "}" -- down
+    | "{" SelfReceiver "," Sender "}" -- up
+    | "{" Receiver "," Sender "}" -- route
+
+  SelfReceiver = kwreceivers ":" "[" "{" kwreceiver ":" SelfPair "}" "]"
+  SelfSender = kwsenders ":" "[" "{" kwsender ":" SelfPair "}" "]"
+  SelfPair = "{" kwcomponent ":" dq "." dq "," kwport ":" PortName "}"
+  Receiver = kwreceivers ":" "[" "{" kwreceiver ":" Pair "}" "]"
+  Sender = kwsenders ":" "[" "{" kwsender ":" Pair "}" "]"
+  SelfPair = "{" kwcomponent ":" ComponentName "," kwport ":" PortName "}"
+
+
+  kwreceivers = dq "receivers" dq
+  kwreceiver = dq "receiver" dq
+  kwsenders = dq "senders" dq
+  kwsender = dq "sender" dq
+  kwcomponent = dq "component" dq
+  kwport = dq "port" dq
+  PortName = nodqstring
+  ComponentName = nodqstring
+
+  nodqstring = dq (~dq any)* dq
+  string = dq (~dq any)* dq
+  dq = "\""
+}
+`;
+
+const fConnections = String.raw`
+Connections {
+  Connections [Connection*] = ‛⟨Connection⟩’
+  Connection_passThrough [lb Receiver kcomma Sender rb] = ‛\n⟨lv⟩Connect (⟨Sender⟩, ⟨Receiver⟩, self.passThrough)⟨rv⟩’
+  Connection_down [lb Receiver kcomma Sender rb] = ‛\n⟨lv⟩Connect (⟨Sender⟩, ⟨Receiver⟩, self.down)⟨rv⟩’
+  Connection_up [lb Receiver kcomma Sender rb] = ‛\n⟨lv⟩Connect (⟨Sender⟩, ⟨Receiver⟩, self.up)⟨rv⟩’
+  Connection_route [lb Receiver kcomma Sender rb] = ‛\n⟨lv⟩Connect (⟨Sender⟩, ⟨Receiver⟩, self.route)⟨rv⟩’
+
+  Receiver [dq1 kreceivers dq2 kcolon1 lbracket lbrace dq3 kreceiver dq4 kcolon2 Pair rbrace rbracket] = ‛Receiver (⟨Pair⟩)’
+  Sender  [dq1 ksenders dq2 kcolon1 lbracket lbrace dq3 ksender dq4 kcolon2 Pair rbrace rbracket] = ‛Sender (⟨Pair⟩)’
+  SelfReceiver [dq1 kreceivers dq2 kcolon1 lbracket lbrace dq3 kreceiver dq4 kcolon2 Pair rbrace rbracket] = ‛Receiver (⟨Pair⟩)’
+  SelfSender  [dq1 ksenders dq2 kcolon1 lbracket lbrace dq3 ksender dq4 kcolon2 Pair rbrace rbracket] = ‛Sender (⟨Pair⟩)’
+
+
+  kwreceivers [a b c] = ‛’
+  kwreceiver [a b c] = ‛’
+  kwsenders [a b c] = ‛’
+  kwsender [a b c] = ‛’
+  kwcomponent [a b c] = ‛’
+  kwport [a b c] = ‛’
+
+
+  PortName [nodqstring] = ‛⟨nodqstring⟩’
+  ComponentName [nodqstring] = ‛⟨nodqstring⟩’
+
+  nodqstring [dq1 any* dq2] = ‛⟨any⟩’
+  string [dq1 any* dq2] = ‛⟨dq1⟩⟨any⟩⟨dq2⟩’
+  dq [q] = ‛⟨c⟩’
+}
+`;
+
+ //    Connection = ":" kreceivers ":" "[" Rec
+ // "{" kreceivers ":" "[" "{" kreceiver ":" "{" kcomponent ":" ComponentName "," kport ":" PortName    
+
+// -------- grammar 
+// Connection := 
+//   | "{" SelfReceiver "," SelfSender "}" -- passThrough
+//   | "{" Receiver "," SelfSender "}" -- down
+//   | "{" SelfReceiver "," Sender "}" -- up
+//   | "{" Receiver "," Sender "}" -- route
+// }
+
+// -------- fmt
+
+// ConnectionBody [lb Connection* optcomma* rb] = ‛⟨Connection⟩’
+
+function fmtConnections (s) {
+    return s;
+}
