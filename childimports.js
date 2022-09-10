d74e7f30ccfd9a2a60519280b35a747224ac3dac
Paul Tarvydas <guitarvydas@gmail.com>
2022-09-10 14:15:42 -0400

    childImports whittled down to small set of overrides

--- childimports.js
+++ childimports.js
@@ -3,7 +3,7 @@ ChildImports <: IdentityEmitter {
 }
 `;
 
-const fChildImports =
+const old_fChildImports =
         fComponents
       + fInsert
       + fVerbatim
@@ -60,7 +60,26 @@ dq [c] = ‛⟨c⟩’
 fOverride {
   Components [vs1 lb vs2 Component+ vs3 rb vs4] = ‛⟨vs1⟩⟨vs2⟩⟨Component⟩⟨vs3⟩⟨vs4⟩’
   ComponentDef [vs1 lb vs2 ComponentJSON vs3 rb vs4 optcomma] = ‛\n⟨vs1⟩⟨vs2⟩⟨ComponentJSON⟩⟨vs3⟩⟨vs4⟩’
+  Child [lb kkind kcolon KindName kcomma kname kcolon ComponentName rb optcomma?] = ‛\nX⟨lv⟩from ⟨KindName⟩ import ⟨KindName⟩⟨rv⟩x’
+}
+`
+;
+
+const fChildImports =
+        fIdentityEmitter
+      + String.raw`
+`
++ `
+fOverride {
+  Components [vs1 lb vs2 Component+ vs3 rb vs4] = ‛⟨vs1⟩⟨vs2⟩⟨Component⟩⟨vs3⟩⟨vs4⟩’
+  Component [SelfDef SelfKind ComponentDef] = ‛\n⟨ComponentDef⟩’
+  ComponentDef [vs1 lb vs2 ComponentJSON vs3 rb vs4 optcomma] = ‛\n⟨vs1⟩⟨vs2⟩⟨ComponentJSON⟩⟨vs3⟩⟨vs4⟩’
   Child [lb kkind kcolon KindName kcomma kname kcolon ComponentName rb optcomma?] = ‛\n⟨lv⟩from ⟨KindName⟩ import ⟨KindName⟩⟨rv⟩’
+  ComponentJSON [x] = ‛⟨x⟩’
+  ComponentContainerJSON [lb NonEmptyChildren ComponentField+ rb] = ‛⟨NonEmptyChildren⟩’
+  ComponentLeafJSON  [lb EmptyChildren ComponentField+ rb] = ‛’
+  NonEmptyChildren [dq1 kchildren dq2 kcolon ChildList optcomma?] = ‛⟨ChildList⟩’
+  ChildList [lb Child* rb] = ‛⟨Child⟩’
 }
 `
 ;
--- class.js
+++ class.js
@@ -1,9 +1,56 @@
-  const gClass = gIdentityEmitter + String.raw`
+  var gClass = gIdentityEmitter + String.raw`
 xClass <: IdentityEmitter {
 }
 `;
 
-var selfKind = '<TBD>';
+var selfKind = ['<TBD>'];
 
-const fClass =
+var fClass =
       fIdentityEmitter;
++ `
+fTest {
+Components [vs1 lb vs2 Component+ vs3 rb vs4] = ‛ZZZ⟨vs1⟩⟨lb⟩⟨vs2⟩⟨Component⟩⟨vs3⟩⟨rb⟩⟨vs4⟩’
+}
+`;
+
+/*fClass {
+  Child [lb kkind kcolon1 KindName kcomma kname kcolon2 ComponentName rb optComma] = ‛A*⟨lb⟩⟨kkind⟩⟨kcolon1⟩⟨KindName⟩⟨kcomma⟩⟨kname⟩⟨kcolon2⟩⟨ComponentName⟩⟨rb⟩⟨optComma⟩a*’
+}
+*/
+
+/* Child formatter
+   parses: {"kind":"Hello","name":"cell_7"},{"kind":"World","name":"cell_8"}
+   used twice:
+   1. parse child string and return
+      cell_7 = Hello (...)
+      cell_8 = World (...)
+
+   2. parse child string and return
+      self.children = [cell_7, cell_8]
+*/
+
+var gSubChildInstantiate = gClass + String.raw`
+ChildInstantiate <: xClass {
+}
+`;
+
+var fSubChildInstantiate = fClass + String.raw`
+fSubChildInstantiate {
+  Child [lb kkind kcolon KindName kcomma kname kcolon ComponentName rb optcomma?] = ‛\n⟨lv⟩⟨ComponentName⟩ = ⟨KindName⟩ (self, f'{name}-⟨KindName⟩');⟨rv⟩’
+}
+`;
+
+
+function fmtChildInstances (text) {
+    let instantiations = '';
+    let childlist = ''
+    let success = true;
+    success && ([success, instantiations, errormessage] = transpile (text, "ChildInstantiate", gSubChildInstantiate, fSubChildInstantiate));
+    if (success) {
+	return instantiations + '\nself._children = [' + childlist + ']';
+    } else {
+	var msg = `??? ${errormessage} ???`;
+	console.error (msg);
+	return msg;
+    }
+}
