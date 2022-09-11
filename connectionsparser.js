a14f7989f6e75d65b10318c6d09e9972da33b22c
Paul Tarvydas <guitarvydas@gmail.com>
2022-09-10 22:48:11 -0400

    rename grammar fSubChildList

--- connectionsparser.js
+++ connectionsparser.js
@@ -7,7 +7,7 @@ ChildList <: xClass {
 var fSubChildList =
       fClass
     + String.raw`
-fSubChildList {
+fSubChildInstantiate {
   Main [child+] = ‛⟨child⟩’
   Child [lb kkind kcolon KindName kcomma kname kcolon ComponentName rb optcomma?] = ‛⟨lv⟩⟨ComponentName⟩,⟨rv⟩’
   string [vs0 dq1 c* dq2 vs1] = ‛⟨vs0⟩⟨c⟩⟨vs1⟩’
