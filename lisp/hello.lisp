62eb3c8f8ffc8e1a022b6b82f3535c86a910ffc7
Paul Tarvydas <guitarvydas@gmail.com>
2022-09-18 06:16:23 -0400

    hello.lisp working, but, with (machine self)

--- lisp/hello.lisp
+++ lisp/hello.lisp
@@ -3,5 +3,6 @@
   (:default-initargs
    :port-handler (make-instance 'PortHandler :port "*" 
 				    :func (lambda (self message)
+(format *standard-output* "~%Hello self ~a" self)
 					    (send self (machine self) "stdout" "hello" message)))))
 
